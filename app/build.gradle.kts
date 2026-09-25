3243cdab2f57462b30cd5613c413aac1d6ebb8ed
eaangrino <eaangrino@gmail.com>
2026-09-24 23:08:29 -0500

    chore: new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.eaangrino.kinewall"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.7.0"
+        versionCode = 7
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
